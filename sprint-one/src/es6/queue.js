a326072a8bba3a014067c609a1978d09f15a8455
Therese Brown <browntm96@gmail.com>
2019-04-24 09:28:01 -0700

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,23 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this;
+  }
+
+  enqueue(val) {
+    if (!this[Object.keys(this).length]) {
+      this[Object.keys(this).length] = val;
+    }
+  }
+
+  dequeue() {
+    let temp = this[Object.keys(this)[0]];
+    delete this[Object.keys(this)[0]];
+    return temp;
+  }
+
+  size() {
+    return Object.keys(this).length;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this;
   }
 
+  push(val) {
+    if (!this[Object.keys(this).length]) {
+      this[Object.keys(this).length] = val;
+    }
+  }
+
+
+  pop() {
+    let temp = this[Object.keys(this).length - 1];
+    delete this[Object.keys(this).length - 1];
+    return temp;
+  }
+
+  size() {
+    return Object.keys(this).length;
+  }
 }
