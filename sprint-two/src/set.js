64d6c9a760e56d1c82c84e12317272777d634544
Therese Brown <browntm96@gmail.com>
2019-04-26 10:23:07 -0700

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage.indexOf(item) === -1) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.indexOf(item) !== -1) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    this._storage.splice(this._storage.indexOf(item), 1);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  add: constant, O(1)
+  contains: constant, O(1)
+  remove: linear, O(n)
  */
