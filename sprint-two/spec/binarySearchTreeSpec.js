320cd75f624fe12e5edcbad3d2bb7a68228d9d63
Therese Brown <browntm96@gmail.com>
2019-04-27 13:05:29 -0700

    Complete Binary Search Tree

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -2,7 +2,7 @@ describe('binarySearchTree', function() {
   var binarySearchTree;
 
   beforeEach(function() {
-    binarySearchTree = BinarySearchTree(5);
+    binarySearchTree = new BinarySearchTree(5);
   });
 
   it('should have methods named "insert", "contains", and "depthFirstLog', function() {
@@ -37,4 +37,35 @@ describe('binarySearchTree', function() {
     binarySearchTree.depthFirstLog(func);
     expect(array).to.eql([5, 2, 3, 7]);
   });
+
+  it('should not add duplicates to the tree', function () {
+    var array = [];
+    var func = function(value) { array.push(value); };
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(7);
+    binarySearchTree.depthFirstLog(func);
+    expect(array).to.eql([5, 2, 3, 7]);
+  });
+
+  it('should be able to handle negative numbers', function() {
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(-7);
+    expect(binarySearchTree.contains(-7)).to.equal(true);
+    expect(binarySearchTree.left.left.value).to.equal(-7);
+  });
+
+  it('should be able to handle decimals', function() {
+    binarySearchTree.insert(2.5);
+    binarySearchTree.insert(3.25);
+    binarySearchTree.insert(-7.89);
+    binarySearchTree.insert(-7.9);
+    expect(binarySearchTree.contains(2.5)).to.equal(true);
+    expect(binarySearchTree.left.right.value).to.equal(3.25);
+    expect(binarySearchTree.left.left.value).to.equal(-7.89);
+    expect(binarySearchTree.left.left.left.value).to.equal(-7.9);
+  });
+
 });
